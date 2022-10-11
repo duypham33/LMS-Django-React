5c529610f9b707352ac9d72a5fb941cfcbf99044
Duypham33 <duy.l.pham@wsu.edu>
2022-10-10 18:42:16 -0700

    Fix test_enroll_course in TestModels!

--- LMS/app/tests/tests_model.py
+++ LMS/app/tests/tests_model.py
@@ -2,7 +2,6 @@
 from app.tests import testbase
 from app.models import User, Course
 from teacher.models import Teacher
-from student.models import Student
 
 class TestViews(testbase.TestBase):
     def setUp(self):
@@ -24,22 +23,32 @@ class TestViews(testbase.TestBase):
 
     # def test_enroll_course(self):
     #     user_teacher = self.create_user('1')
-    #     t = user_teacher.get_role()
+    #     t = user_teacher.teacher
 
     #     user_student = User.objects.create(username="test name2", email="test2@gmail.com", 
     #     user_type = str('3'), password = self.user["password"])
-    #     s = user_student.get_role()
+    #     s = user_student.student
 
+    #     user_staff = User.objects.create(username="test name3", email="test3@gmail.com", 
+    #     user_type = str('2'), password = self.user["password"])
+    #     sta = user_staff.staff
+
+    #     #Create course for teacher
     #     c = Course.objects.create(instructor = t, title="New Course", coursenum="321", syllabus='',
     #     price = 100.00, discount = 30)
 
     #     self.assertEquals(c, t.courses.first())
     #     self.assertAlmostEquals(c.final_price(), 70.00)
 
+
+    #     #Add staff to course
+    #     t.edit_course_list(thePerson = sta, courseID_list = [c.id])
+    #     self.assertIn(sta, c.staffs.all())
+    #     self.assertIn(c, sta.courses.all())
+
     #     #Enroll
     #     s.enroll(c)
     #     self.assertEquals(s, c.students.first())
     #     self.assertEquals(c, s.courses.first())
-    #     # t.refresh_from_db()  
-    #     # self.assertEquals(s, t.students.first())   #Not work since Django test not apply signal
+    #     self.assertEquals(s, sta.students.first())   #Because of signal
         
