df8c69f568288be72144d49c92f81789d33e5d65
duypham33 <duy.l.pham@wsu.edu>
2022-10-27 19:55:00 -0700

    Merge main branch and chatnoticdev branch!

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -83,20 +83,20 @@ TEMPLATES = [
 WSGI_APPLICATION = "LMS.wsgi.application"
 ASGI_APPLICATION = "LMS.asgi.application"
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
-# }
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     }
+# }
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
